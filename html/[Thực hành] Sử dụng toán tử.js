250dc8915f5796122f50554a05120a14210c0d2f
nguyenduccanh120394 <duccanhh94@gmail.com>
2021-04-07 16:00:15 +0700

    git add .

---
+++ html/[Thực hành] Sử dụng toán tử.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+let width = parseInt(inputWidth);//Sử dụng hàm parseInt() để chuyển kiểu dữ liệu từ chuỗi sang số nguyên.
+let height = parseInt(inputHeight);//Sử dụng hàm parseInt() để chuyển kiểu dữ liệu từ chuỗi sang số nguyên.
+let s = inputHeight*inputWidth;
+alert('Diện Tích là:'+s);
---
+++ html/index.js
@@ -0,0 +1,3 @@
+function login(){
+    document.write("Bạn đã đăng nhập");
+}
